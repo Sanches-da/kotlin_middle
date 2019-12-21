5c866e4be7fb4c38a23b1013be9dde362bf56ed7
Sanches_da <dudnik2104@gmail.com>
2019-12-21 20:22:16 +0200

    hometask_2

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.kotlinexample.extensions
 
-fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T>{
-    return this.dropLastWhile { !predicate(it) }.dropLastWhile(predicate)
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): ArrayList<T>{
+    val res = ArrayList<T>()
+    res.addAll(this.dropLastWhile { !predicate(it) }.dropLastWhile(predicate))
+    return res
 }
